a52ec5dafff3f37caafa627287b49108a8e75d9f
charokoukuu <charokoukuu@gmail.com>
2024-04-11 19:16:02 +0900

    動く状態にした

--- printer/src/pos.py
+++ printer/src/pos.py
@@ -6,7 +6,6 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import datetime
-import hashlib
 import list
 from enum import Enum
 import cv2
@@ -43,33 +42,32 @@ def DrawText(mode, title, size):
             fontPath, int(round(size)), encoding='unic'), fill=0)
 
 
-for title in menuTitle:
-    fontSize = float(50)
-    image = Image.new('1', (width, height), 255)
-    draw = ImageDraw.Draw(image)
+fontSize = float(50)
+image = Image.new('1', (width, height), 255)
+draw = ImageDraw.Draw(image)
 
-    DrawText(Mode.UP, "大阪工業大学", fontSize)
-    font = ImageFont.truetype(
-        fontPath, 20, encoding='unic')
-    draw.text((210, 1050), date+" "+timeStr, font=font, fill=0)
-    draw.text((210, 1070), "ID: "+orderId, font=font, fill=0)
+DrawText(Mode.UP, "大阪工業大学", fontSize)
+font = ImageFont.truetype(
+    fontPath, 20, encoding='unic')
+draw.text((210, 1050), date+" "+timeStr, font=font, fill=0)
+draw.text((210, 1070), "ID: "+orderId, font=font, fill=0)
 
-    img_path = f"/home/kiosk/air-draw/printer/src/image/{title}.png"
-    try:
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+img_path = f"./image/imgcar.png"
+try:
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-        img = cv2.resize(img, (420, 990))
-        img = cv2.GaussianBlur(img, (39, 39), 0)
-        threshold = 128
-        _, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-        img_pil = Image.fromarray(img)
-        image.paste(img_pil, (0, 0))
+    img = cv2.resize(img, (420, 990))
+    # img = cv2.GaussianBlur(img, (39, 39), 0)
+    threshold = 128
+    _, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+    img_pil = Image.fromarray(img)
+    image.paste(img_pil, (0, 0))
 
-        # image.save('pillow_imagedraw.jpg', quality=95)
-    except FileNotFoundError:
-        print(f"エラー：{img_path}に画像が見つかりませんでした。")
+    image.save('pillow_imagedraw.jpg', quality=95)
+except FileNotFoundError:
+    print(f"エラー：{img_path}に画像が見つかりませんでした。")
 
-    p = escpos.printer.Serial("/dev/ttyACM0")
-    p.image(image)
-    p.cut()
-    time.sleep(1)
+p = escpos.printer.Serial("/dev/ttyACM0")
+p.image(image)
+p.cut()
+time.sleep(1)
--- printer/src/qr.sh
+++
@@ -1,2 +0,0 @@
-ip_address=$(ip a | awk '/inet /{print $2}' | awk -F/ 'NR==2{print $1}')
-python3 /home/kiosk/RunTicket/printer/src/qr.py "$ip_address"
