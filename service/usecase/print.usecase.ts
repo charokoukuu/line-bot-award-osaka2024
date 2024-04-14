0bfe26657aa9645461e44ae8aa9d45af9dd3a25b
charokoukuu <charokoukuu@gmail.com>
2024-04-14 11:57:20 +0900

    Firebase APIのヒントメッセージを変更

--- service/usecase/print.usecase.ts
+++ service/usecase/print.usecase.ts
@@ -5,7 +5,16 @@ export const HintPrintService = async (req: Request, res: Response) => {
   const id = req.query.id as string;
   await hintPrint(id, req.query.text as string);
   const base64 = encodePNGToBase64(id) as string;
-  console.log(base64);
+  await fetch("https://pos.run-ticket.com/hint?name=2", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // Content-Typeを設定
+    },
+    body: JSON.stringify({
+      id: id,
+      base64: base64,
+    }),
+  });
   // アップロードする処理
   res.status(200).send("HintPrintService");
 };
