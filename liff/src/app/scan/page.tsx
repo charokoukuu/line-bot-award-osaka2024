eef32e6d934446ea59efa736fd7b3624c709edcc
えんどー <64085231+Nikudanngo@users.noreply.github.com>
2024-04-30 14:36:54 +0900

    チーム作成画面 (#24)
    * add useEffect
    
    * createTeamのデータ構造変更
    
    - useEffectによるフォームの連動変更
    
    * CreateTeam -> Team
    
    * rename
    
    - 隠す人 -> オーナー
    - 探す人 -> シーカー
    
    ---------
    
    Co-authored-by: charokoukuu <charokoukuu@gmail.com>

--- liff/src/app/scan/page.tsx
+++ liff/src/app/scan/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 import { useLiff } from "@/components/LiffProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function Scan() {
   const [keyword, setKeyword] = useState("");
   const [isSaned, setIsSaned] = useState(false);
   const { liff } = useLiff();
-  if (!isSaned && keyword === "") {
-    liff?.scanCodeV2().then(async (result) => {
-      console.log(result.value);
-      setKeyword(result.value ?? "");
-      fetch(
-        `https://local-line.run-ticket.com/qr-scan?id=${liff.id}&content=${result.value}`,
-        {
-          method: "GET",
-        }
-      ).finally(() => {
-        setIsSaned(true);
+  useEffect(() => {
+    if (!isSaned && keyword === "") {
+      liff?.scanCodeV2().then(async (result) => {
+        console.log(result.value);
+        setKeyword(result.value ?? "");
+        fetch(
+          `https://local-line.run-ticket.com/qr-scan?id=${liff.id}&content=${result.value}`,
+          {
+            method: "GET",
+          }
+        ).finally(() => {
+          setIsSaned(true);
+        });
       });
-    });
-  }
+    }
+  }, [isSaned, keyword, liff]);
   return (
     <main className="m-4">
       <h1 className="text-center text-3xl font-semibold">スキャン</h1>
