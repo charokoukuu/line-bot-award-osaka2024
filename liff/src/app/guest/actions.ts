ed844926bfcbc1adcd19a5af5a3c2077689f3aaa
Nikudanngo <satoru_rutosa@icloud.com>
2024-04-30 17:02:22 +0900

    add join team

---
+++ liff/src/app/guest/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+export async function teamJoin(userId: string, teamId: string) {
+  const req = {
+    "userId": userId,
+    "teamId": teamId,
+  };
+  console.log(req);
+  const res = await fetch("https://node-learn.run-ticket.com/api/team-joining", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ req }),
+  }).then((res) => {
+    console.log(res);
+    if (res.status === 200) {
+      console.log("success");
+    } else {
+      console.error("error");
+    }
+  }).catch((error) => {
+    console.error(error);
+  }).finally(() => {
+    console.log("done");
+  });
+  return res;
+}
--- liff/src/type/host.ts
+++ liff/src/type/host.ts
@@ -1,6 +1,16 @@
-export type Team = {
+export type CreateTeam = {
   userId: string;
   teamName: string,
+  playerCount: number,
+  ownerCount: number,
+  treasureCount: number,
+  keyword: string,
+};
+
+export type JoinTeam = {
+  teamId: string,
+  hostId: string,
+  name: string,
   playerCount: number,
   ownerCount: number,
   treasureCount: number,
