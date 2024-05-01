4f7490655a24a44440f0dbb672f3ab9f967a31a5
nnikudango <nnikudango@gmail.com>
2024-05-01 20:48:41 +0900

    test scan

--- liff/src/app/scan/page.tsx
+++ liff/src/app/scan/page.tsx
@@ -3,74 +3,78 @@ import { useLiff } from "@/components/LiffProvider";
 import { useEffect, useState } from "react";
 export default function Scan() {
   const [isSaned, setIsSaned] = useState(false);
+  const [isFetchScannerStatus, setIsFetchScannerStatus] = useState(false);
+  const [userId, setUserId] = useState("");
   const [scannerStatus, setScannerStatus] = useState<{
     userId: string;
     isDisabledScan: boolean;
   } | null>(null);
   const { liff, profile } = useLiff();
+
   useEffect(() => {
-    console.log("scan useEffect");
     if (!profile || !liff) return;
-    if (!scannerStatus?.isDisabledScan) {
-      fetch(
-        `https://node-learn.run-ticket.com/api/scannerStatus/${profile.userId}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setScannerStatus(data);
-        });
-    }
-    if (!isSaned) {
-      const userId = profile.userId;
-      liff?.scanCodeV2().then(async (result) => {
-        console.log(result.value);
-        fetch("https://node-learn.run-ticket.com/api/qr-scan", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            userId,
-            qrCode: result.value,
-          }),
-        })
-          .then((res) => {
-            console.log(res);
-          })
-          .finally(() => {
-            setIsSaned(true);
-            liff.closeWindow();
-          });
+    const userId = profile.userId;
+    setUserId(userId);
+
+    console.log("scan useEffect");
+    if (scannerStatus?.isDisabledScan || isFetchScannerStatus) return;
+    fetch(`https://node-learn.run-ticket.com/api/scannerStatus/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setScannerStatus(data);
+      })
+      .finally(() => {
+        setIsFetchScannerStatus(true);
       });
-    }
-  }, [isSaned, liff, profile, scannerStatus?.isDisabledScan]);
+    console.log("scan useEffect status");
+  }, [liff, profile, scannerStatus?.isDisabledScan, isFetchScannerStatus]);
+
+  useEffect(() => {
+    if (isSaned || !liff || !userId) return;
+    liff?.scanCodeV2().then(async (result) => {
+      console.log(result.value);
+      fetch("https://node-learn.run-ticket.com/api/qr-scan", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId,
+          qrCode: result.value,
+        }),
+      })
+        .then((res) => {
+          console.log(res);
+        })
+        .finally(() => {
+          setIsSaned(true);
+          liff.closeWindow();
+        });
+    });
+  }, [isSaned, liff, userId]);
 
   return (
     <main className="m-4">
       {/* <h1 className="text-center text-3xl font-semibold">スキャン</h1> */}
-      <div className="flex flex-col w-full h-full justify-center items-center">
-        {isSaned ? (
+      <div className="flex flex-col w-full h-full justify-center items-center gap-5">
+        {scannerStatus?.isDisabledScan ? (
+          <p className="text-center text-xl">スキャンが無効化されています</p>
+        ) : (
           <>
-            <p className="text-center text-2xl">スキャンが完了しました</p>
-            {scannerStatus?.isDisabledScan ? (
-              <p className="text-center text-2xl">
-                スキャンが無効化されています
-              </p>
+            {isSaned ? (
+              <>
+                <p className="text-center text-2xl">スキャンが完了しました</p>
+              </>
             ) : (
-              <p className="text-center text-2xl">
-                スキャンが有効化されています
-              </p>
+              <p className="text-center text-2xl">スキャン中...</p>
             )}
           </>
-        ) : (
-          <p className="text-center text-2xl">スキャン中...</p>
         )}
       </div>
     </main>
