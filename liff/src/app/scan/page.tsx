15b21a2972f58c7587fefc0b7feb6a637738a5fe
nnikudango <nnikudango@gmail.com>
2024-05-01 20:12:56 +0900

    test scan disable

--- liff/src/app/scan/page.tsx
+++ liff/src/app/scan/page.tsx
@@ -3,26 +3,44 @@ import { useLiff } from "@/components/LiffProvider";
 import { useEffect, useState } from "react";
 export default function Scan() {
   const [isSaned, setIsSaned] = useState(false);
+  const [scannerStatus, setScannerStatus] = useState<{
+    userId: string;
+    isDisabledScan: boolean;
+  } | null>(null);
   const { liff, profile } = useLiff();
   useEffect(() => {
-    if (!isSaned && liff && profile) {
+    console.log("scan useEffect");
+    if (!profile || !liff) return;
+    if (!scannerStatus?.isDisabledScan) {
+      fetch(
+        `https://node-learn.run-ticket.com/api/scannerStatus/${profile.userId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setScannerStatus(data);
+        });
+    }
+    if (!isSaned) {
       const userId = profile.userId;
       liff?.scanCodeV2().then(async (result) => {
         console.log(result.value);
-        fetch(
-          // `https://local-line.run-ticket.com/qr-scan?id=${userId}&content=${result.value}`,
-          "https://node-learn.run-ticket.com/api/qr-scan",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              userId,
-              qrCode: result.value,
-            }),
-          }
-        )
+        fetch("https://node-learn.run-ticket.com/api/qr-scan", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userId,
+            qrCode: result.value,
+          }),
+        })
           .then((res) => {
             console.log(res);
           })
@@ -32,7 +50,7 @@ export default function Scan() {
           });
       });
     }
-  }, [isSaned, liff, profile]);
+  }, [isSaned, liff, profile, scannerStatus?.isDisabledScan]);
 
   return (
     <main className="m-4">
@@ -41,6 +59,15 @@ export default function Scan() {
         {isSaned ? (
           <>
             <p className="text-center text-2xl">スキャンが完了しました</p>
+            {scannerStatus?.isDisabledScan ? (
+              <p className="text-center text-2xl">
+                スキャンが無効化されています
+              </p>
+            ) : (
+              <p className="text-center text-2xl">
+                スキャンが有効化されています
+              </p>
+            )}
           </>
         ) : (
           <p className="text-center text-2xl">スキャン中...</p>
