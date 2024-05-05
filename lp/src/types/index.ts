825a96e5bf6eeaa3d3a14065166e22078acb9311
nnikudango <nnikudango@gmail.com>
2024-05-05 12:03:05 +0900

    add rule page

--- lp/src/types/index.ts
+++ lp/src/types/index.ts
@@ -9,16 +9,7 @@ export const navigationItem: navigationItemProps[] = [
     path: "/",
   },
   {
-    name: "利用規約",
-    path: "/terms",
-  },
-  {
-    name: "プライバシーポリシー",
-    path: "/privacy",
-  },
-  {
-    name: "お問い合わせ",
-    // path: "/contact",
-    path: "https://docs.google.com/forms/d/e/1FAIpQLSfRRIK0WBAoMt_WN3RAKbP598LZOQAhsOrIQu8O7eAZE81x1Q/viewform",
+    name: "ゲームルール",
+    path: "/rule",
   },
 ];
