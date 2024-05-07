2e93905cc7718700085443462b76269cec770854
Nikudanngo <satoru_rutosa@icloud.com>
2024-05-07 15:32:26 +0900

    fix OGP image

--- lp/src/pages/_app.tsx
+++ lp/src/pages/_app.tsx
@@ -11,29 +11,29 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>RunTicket LP</title>
+        <title>BOTREASURE LP</title>
         <meta
           name="description"
-          content="学内モバイルオーダー「RunTicket」でスピーディーな注文をしてみませんか？PayPay・クレジット決済・Apple Payなど、様々な支払い方法に対応しています。"
+          content="BOTREASUREは、オーナーとシーカーに分かれた参加者が宝を巡って競い合うゲームです。オーナーは宝を隠し、シーカーはそれを見つけ出します。双方が戦略と推理を駆使して勝利を目指します。さぁ今すぐ冒険を始めよう"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/PulluP-mini-logo.svg" />
         <meta name="theme-color" content="#000000" />
-        <meta property="og:title" content="RunTicket LP" />
+        <meta property="og:title" content="BOTREASURE LP" />
         <meta
           property="og:description"
-          content="学内モバイルオーダー「RunTicket」でスピーディーな注文をしてみませんか？PayPay・クレジット決済・Apple Payなど、様々な支払い方法に対応しています。"
+          content="BOTREASUREは、オーナーとシーカーに分かれた参加者が宝を巡って競い合うゲームです。オーナーは宝を隠し、シーカーはそれを見つけ出します。双方が戦略と推理を駆使して勝利を目指します。さぁ今すぐ冒険を始めよう"
         />
-        <meta property="og:url" content="https://run-ticket.com" />
+        <meta property="og:url" content="https://botreasure.com" />
         <meta
           property="og:image"
-          content="https://firebasestorage.googleapis.com/v0/b/runticket-6c269.appspot.com/o/OGP%2Fthumbnail-run-ticket.png?alt=media"
+          content="https://firebasestorage.googleapis.com/v0/b/line-bot-award-osaka2024.appspot.com/o/assets%2FBOTREASURE_ogp.png?alt=media&token=a3b6fa12-2a3c-4ed7-8916-5e6ba0370b73"
         />
         <meta property="og:type" content="website" />
         <meta name="twitter:site" content="@runticket21" />
         <meta
           name="twitter:image"
-          content="https://firebasestorage.googleapis.com/v0/b/runticket-6c269.appspot.com/o/OGP%2Fthumbnail-run-ticket.png?alt=media"
+          content="https://firebasestorage.googleapis.com/v0/b/line-bot-award-osaka2024.appspot.com/o/assets%2FBOTREASURE_ogp.png?alt=media&token=a3b6fa12-2a3c-4ed7-8916-5e6ba0370b73"
         />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
